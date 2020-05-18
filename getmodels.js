c213fdf0c987ed13b44f68e42a688fed0f8b9009
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2020-05-18 14:37:58 +0900

    Add handling to retrieve model files

--- getmodels.js
+++ getmodels.js
@@ -1,21 +1,40 @@
 var fs = require('fs');
 var superagent = require('superagent');
-var urls = [
-    'https://storage.googleapis.com/tfjs-models/savedmodel/posenet/mobilenet/float/075/model-stride16.json',
-    'https://storage.googleapis.com/tfjs-models/savedmodel/posenet/mobilenet/float/075/group1-shard1of2.bin',
-    'https://storage.googleapis.com/tfjs-models/savedmodel/posenet/mobilenet/float/075/group1-shard2of2.bin'
-];
+var models = [{
+    name: 'mobilenet',
+    urls: ['https://storage.googleapis.com/tfhub-tfjs-modules/google/imagenet/mobilenet_v1_100_224/classification/1/model.json',
+           'https://storage.googleapis.com/tfhub-tfjs-modules/google/imagenet/mobilenet_v1_100_224/classification/1/group1-shard1of5',
+           'https://storage.googleapis.com/tfhub-tfjs-modules/google/imagenet/mobilenet_v1_100_224/classification/1/group1-shard2of5',
+           'https://storage.googleapis.com/tfhub-tfjs-modules/google/imagenet/mobilenet_v1_100_224/classification/1/group1-shard3of5',
+           'https://storage.googleapis.com/tfhub-tfjs-modules/google/imagenet/mobilenet_v1_100_224/classification/1/group1-shard4of5',
+           'https://storage.googleapis.com/tfhub-tfjs-modules/google/imagenet/mobilenet_v1_100_224/classification/1/group1-shard5of5']
+}, {
+    name: 'coco-ssd',
+    urls: ['https://storage.googleapis.com/tfjs-models/savedmodel/ssdlite_mobilenet_v2/model.json',
+           'https://storage.googleapis.com/tfjs-models/savedmodel/ssdlite_mobilenet_v2/group1-shard1of5',
+           'https://storage.googleapis.com/tfjs-models/savedmodel/ssdlite_mobilenet_v2/group1-shard2of5',
+           'https://storage.googleapis.com/tfjs-models/savedmodel/ssdlite_mobilenet_v2/group1-shard3of5',
+           'https://storage.googleapis.com/tfjs-models/savedmodel/ssdlite_mobilenet_v2/group1-shard4of5',
+           'https://storage.googleapis.com/tfjs-models/savedmodel/ssdlite_mobilenet_v2/group1-shard5of5']
+}, {
+    name: 'posenet',
+    urls: ['https://storage.googleapis.com/tfjs-models/savedmodel/ssdlite_mobilenet_v2/group1-shard5of5',
+           'https://storage.googleapis.com/tfjs-models/savedmodel/posenet/mobilenet/float/075/model-stride16.json',
+           'https://storage.googleapis.com/tfjs-models/savedmodel/posenet/mobilenet/float/075/group1-shard1of2.bin',
+           'https://storage.googleapis.com/tfjs-models/savedmodel/posenet/mobilenet/float/075/group1-shard2of2.bin']
+}];
 
-fs.mkdir(__dirname + '/models', function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        for (var i = 0; i < urls.length; i++) {
-            superagent.get(urls[i]).responseType('blob').end(function (err, res) {
+try { fs.mkdirSync(__dirname + '/models'); } catch (e) {}
+models.forEach(function (model) {
+    try { fs.mkdirSync(__dirname + '/models/' + model.name); } catch (e) {}
+    for (var i = 0; i < model.urls.length; i++) {
+        superagent.get(model.urls[i]).responseType('blob').end(function (err, res) {
+            if (!err) {
                 var file = res.req.path.split('/').slice(-1)[0];
-                fs.writeFileSync(__dirname + '/models/' + file, res.body);
-            });
-        }
+                fs.writeFileSync(__dirname + '/models/' + model.name + '/' + file, res.body);
+            } else {
+                console.log(err);
+            }
+        });
     }
 });
-
